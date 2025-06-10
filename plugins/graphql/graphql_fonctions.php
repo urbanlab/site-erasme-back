b3720eb48dc83b05baba55fcc28176f1688bf856
Pierre KUHN <webmaster@pierre-kuhn.org>
2025-06-10 15:29:18 +0200

    feat: utiliser X-Auth-Token

--- plugins/graphql/graphql_fonctions.php
+++ plugins/graphql/graphql_fonctions.php
@@ -11,8 +11,8 @@ function graphql_verif_jeton($headers) {
 	}
 
 	$token_envoye = '';
-	if (isset($headers['X_AUTH_TOKEN'])) {
-		$token_envoye = $headers['X_AUTH_TOKEN'];
+	if (isset($headers['X-Auth-Token'])) {
+		$token_envoye = $headers['X-Auth-Token'];
 	}
 
 	return $token_envoye == $token_param;
