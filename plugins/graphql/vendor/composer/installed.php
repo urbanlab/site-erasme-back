74aa40bc07e6dcd930080d56d0d74370237bcefd
Pierre KUHN <webmaster@pierre-kuhn.org>
2025-06-18 13:48:25 +0200

    feat: maj du vendor

--- plugins/graphql/vendor/composer/installed.php
+++ plugins/graphql/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'dbb0b99b4078bd58f582776107d97ba484febad8',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9c5670e87a863c2ba9d5d7e0584af358fcf794a8',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'dbb0b99b4078bd58f582776107d97ba484febad8',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9c5670e87a863c2ba9d5d7e0584af358fcf794a8',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
