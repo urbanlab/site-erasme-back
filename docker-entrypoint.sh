a10fa6e51366bc5979f2125fe7338017903575b8
Pierre KUHN <webmaster@pierre-kuhn.org>
2025-06-23 15:44:43 +0200

    feat: ordre des plugins

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -108,8 +108,8 @@ fi
 #spip plugins:activer linkcheck -y
 spip plugins:activer simplog -y
 spip plugins:activer cextra -y
-spip plugins:activer erasme -y
 spip plugins:activer saisies -y
+spip plugins:activer erasme -y
 spip plugins:activer yaml -y
 spip plugins:activer graphql -y
 spip plugins:maj:bdd
