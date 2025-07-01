73d84e891bfd7d4509102c5513a40e8e69851d22
Pierre KUHN <webmaster@pierre-kuhn.org>
2025-07-01 13:44:55 +0200

    fix: ordre des plugins

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -107,9 +107,9 @@ fi
 #spip plugins:activer nospam -y
 #spip plugins:activer linkcheck -y
 spip plugins:activer simplog -y
-spip plugins:activer cextra -y
 spip plugins:activer saisies -y
 spip plugins:activer yaml -y
+spip plugins:activer cextra -y
 spip plugins:activer urls_par_numero -y
 spip plugins:activer erasme -y
 spip plugins:activer graphql -y
