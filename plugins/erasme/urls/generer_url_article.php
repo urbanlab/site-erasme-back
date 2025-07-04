1723fa05b5282af5c9e02791f6c9ef73c4e0f85c
Pierre KUHN <webmaster@pierre-kuhn.org>
2025-07-04 14:09:35 +0200

    feat: modifier la génération d'url

--- plugins/erasme/urls/generer_url_article.php
+++ plugins/erasme/urls/generer_url_article.php
@@ -4,7 +4,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
-include_spip('inc/config');
+include_spip('action/editer_url');
 /**
  * Génération rapide de l'url d'un article, sans même en base : retourner l'id
  * @param int $id_article,
@@ -13,5 +13,8 @@ include_spip('inc/config');
  * @return string
 **/
 function urls_generer_url_article(int $id_article, $args = '', $ancre = ''): string {
-	return _DIR_RACINE . $id_article . ($args ? "?$args" : '') . ($ancre ? "#$ancre" : '');
+	$articles = sql_fetsel('*', 'spip_articles', 'id_article=' . $id_article);
+	$secteur = sql_getfetsel('titre', 'spip_rubriques', 'id_rubrique='.$articles['id_secteur']);
+	$secteur = url_nettoyer($secteur, _URLS_PROPRES_MAX, _URLS_PROPRES_MIN, '-', \_url_minuscules ? 'spip_strtolower' : '' );
+	return _DIR_RACINE . $secteur.'/'.$id_article . ($args ? "?$args" : '') . ($ancre ? "#$ancre" : '');
 }
