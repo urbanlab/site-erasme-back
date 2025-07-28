fc920c37095ddcfa875255d0525243870444fb97
Pierre KUHN <webmaster@pierre-kuhn.org>
2025-07-28 13:48:48 +0200

    feat: ne pas activé le plugins erasme

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -111,9 +111,9 @@ spip plugins:activer saisies -y
 spip plugins:activer yaml -y
 spip plugins:activer cextra -y
 spip plugins:activer correction_liens_internes -y
-spip plugins:activer erasme -y
 spip plugins:activer identifiants -y
 spip plugins:activer graphql -y
+#spip plugins:activer erasme -y
 spip plugins:maj:bdd
 
 # Default mes_options
