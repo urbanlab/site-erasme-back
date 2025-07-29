4341b3c94c0e83b83520f2e8d15418dd908d2700
Pierre KUHN <webmaster@pierre-kuhn.org>
2025-07-29 15:27:06 +0200

    feat: rajouter un pipeline pour mettre à jour l'identifiant avec l'article

--- plugins/erasme/era_pipelines.php
+++ plugins/erasme/era_pipelines.php
@@ -13,3 +13,14 @@ function era_taches_generales_cron($taches) {
 	$taches['generer_slug'] = 300; // toutes les 5 minutes on regarde si il y a des sites à tester
 	return $taches;
 }
+
+function era_post_edition($flux) {
+	// Cas des rubriques : création
+	if (isset($flux['args']['table']) && $flux['args']['table'] === 'spip_articles'
+		&& isset($flux['args']['action']) && $flux['args']['action'] === 'modifier'
+		&& isset($flux['args']['id_objet'])
+	) {
+		$article = sql_fetsel('*', $flux['args']['table'], 'id_article=' . intval($flux['args']['id_objet']));
+		sql_updateq($flux['args']['table'], ['identifiant' => identifiant_slug($article['titre'])], 'id_article=' . intval($flux['args']['id_objet']));
+	}
+}
