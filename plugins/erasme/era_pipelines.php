492353d0743375646ff4b50ea921b1991440c29e
Pierre KUHN <webmaster@pierre-kuhn.org>
2025-07-29 16:47:38 +0200

    feat: mettre à jour les rubriques et mots-cles

--- plugins/erasme/era_pipelines.php
+++ plugins/erasme/era_pipelines.php
@@ -15,12 +15,25 @@ function era_taches_generales_cron($taches) {
 }
 
 function era_post_edition($flux) {
-	// Cas des rubriques : création
 	if (isset($flux['args']['table']) && $flux['args']['table'] === 'spip_articles'
 		&& isset($flux['args']['action']) && $flux['args']['action'] === 'modifier'
 		&& isset($flux['args']['id_objet'])
 	) {
 		$article = sql_fetsel('*', $flux['args']['table'], 'id_article=' . intval($flux['args']['id_objet']));
 		sql_updateq($flux['args']['table'], ['identifiant' => identifiant_slug($article['titre'])], 'id_article=' . intval($flux['args']['id_objet']));
+	}
+	if (isset($flux['args']['table']) && $flux['args']['table'] === 'spip_rubriques'
+		&& isset($flux['args']['action']) && $flux['args']['action'] === 'modifier'
+		&& isset($flux['args']['id_objet'])
+	) {
+		$rubrique = sql_fetsel('*', $flux['args']['table'], 'id_rubrique=' . intval($flux['args']['id_objet']));
+		sql_updateq($flux['args']['table'], ['identifiant' => identifiant_slug($rubrique['titre'])], 'id_rubrique=' . intval($flux['args']['id_objet']));
+	}
+	if (isset($flux['args']['table']) && $flux['args']['table'] === 'spip_mots'
+		&& isset($flux['args']['action']) && $flux['args']['action'] === 'modifier'
+		&& isset($flux['args']['id_objet'])
+	) {
+		$mot = sql_fetsel('*', $flux['args']['table'], 'id_mot=' . intval($flux['args']['id_objet']));
+		sql_updateq($flux['args']['table'], ['identifiant' => identifiant_slug($mot['titre'])], 'id_mot=' . intval($flux['args']['id_objet']));
 	}
 }
--- plugins/erasme/genie/generer_slug.php
+++ plugins/erasme/genie/generer_slug.php
@@ -10,5 +10,13 @@ function genie_generer_slug($time) {
 	foreach ($articles as $article) {
 		sql_updateq('spip_articles', ['identifiant' => identifiant_slug($article['titre'])], 'id_article=' . intval($article['id_article']));
 	}
+	$rubriques = sql_allfetsel('*', 'spip_rubriques', ['identifiant = ""'],'','','0,100');
+	foreach ($rubriques as $rubrique) {
+		sql_updateq('spip_rubriques', ['identifiant' => identifiant_slug($rubrique['titre'])], 'id_rubrique=' . intval($article['id_rubrique']));
+	}
+	$mots = sql_allfetsel('*', 'spip_mots', ['identifiant = ""'],'','','0,100');
+	foreach ($mots as $mot) {
+		sql_updateq('spip_mots', ['identifiant' => identifiant_slug($mot['titre'])], 'id_mot=' . intval($article['id_mot']));
+	}
 	return 0;
 }
